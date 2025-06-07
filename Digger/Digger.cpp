f296c3cebfdf9a79ea6b06c99ec7c04c1930051e
DonanCamerlynck <95361547+DonanCamerlynck@users.noreply.github.com>
2025-06-07 20:31:08 +0200

    made static library from engine and added digger project

---
+++ Digger/Digger.cpp
@@ -0,0 +1,19 @@
+#if _DEBUG
+#if __has_include(<vld.h>)
+#include <vld.h>
+#endif
+#endif
+
+#include "Minigin.h"
+
+void load()
+{
+
+}
+
+int main(int, char* [])
+{
+	dae::Minigin engine("../Data/");
+	engine.Run(load);
+	return 0;
+}
